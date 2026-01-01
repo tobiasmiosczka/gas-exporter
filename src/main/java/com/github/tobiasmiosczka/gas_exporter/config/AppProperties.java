f403692e7e63fd9b648aef18438a4015871a13c5
Tobias Miosczka <6351397+tobiasmiosczka@users.noreply.github.com>
2026-01-01 17:02:38 +0100

    fixed missing prefix

--- src/main/java/com/github/tobiasmiosczka/gas_exporter/config/AppProperties.java
+++ src/main/java/com/github/tobiasmiosczka/gas_exporter/config/AppProperties.java
@@ -1,13 +1,14 @@
 package com.github.tobiasmiosczka.gas_exporter.config;
 
 import com.github.tobiasmiosczka.gas_exporter.model.Position;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
 
 @Component
+@ConfigurationProperties(prefix = "app")
 public class AppProperties {
-
 
     private Set<Position> positions;
 
